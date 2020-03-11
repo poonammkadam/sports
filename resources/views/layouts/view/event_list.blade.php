81300248497068ec4f2b2779e6cfc22d572cc24a
poonammkadam <poonammk97@gmail.com>
2020-03-11 01:50:41 -0400

    event list date issue reg date condition applye"

--- resources/views/layouts/view/event_list.blade.php
+++ resources/views/layouts/view/event_list.blade.php
@@ -7,6 +7,7 @@
             </div>
             <div class="row">
                 @foreach($arrObjEvents as $objEvent)
+                @if( date('Y-m-d', strtotime($objEvent->registration_end_date)) > date('Y-m-d'))
                 <div class="shadow card col-md-2 item-inner">
                      <img src="{{asset('images/img3.jpeg')}}" class="card-img-top" alt="">
                     <div class="card-body">
@@ -15,10 +16,28 @@
                             </div>
                                   <h5 class="card-title"><strong>{{$objEvent->name}}</strong></h5>
                         @if(auth()->user()->role != 'organizar')
-                    <a class="btn btn-success btn-block" href="{{url('/event/register').'/'.$objEvent->id}}">Register</a>
+                    <a class="btn btn-success btn-block" href="{{url('/event/register').'/'.$objEvent->id}}" >Register</a>
                             @endif
-                    </div>
+                            <h5 class="card-title">Registration will Close :-{{  date('l j F Y', strtotime($objEvent->registration_end_date))}}</h5>
+                        
+                        </div>
                 </div>
+                @else
+                <div class="shadow card col-md-2 item-inner event-exp">
+                    <img src="{{asset('images/img3.jpeg')}}" class="card-img-top" alt="">
+                   <div class="card-body">
+                           <div class="overlay">
+                               <br><br>
+                           </div>
+                                 <h5 class="card-title"><strong>{{$objEvent->name}}</strong></h5>
+                       @if(auth()->user()->role != 'organizar')
+                   <a class="btn btn-success btn-block" href="{{url('/event/register').'/'.$objEvent->id}}" disabled='disabled'>Register</a>
+                           @endif
+                           <h5 class="card-title">Registration Close :-{{  date('l j F Y', strtotime($objEvent->registration_end_date))}}</h5>
+                    
+                       </div>
+               </div>
+               @endif
                 @endforeach
               </div>
         </div>
