e558a465c531ff287da6e626df96bfafc8d81d30
adityamonad <aditya.chouhan@monadinfotech.com>
2020-03-15 21:37:51 +0530

    add receipt

--- resources/views/mail/receipt.blade.php
+++ resources/views/mail/receipt.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
-    Hi {{($objData['user'])->name}}
-
-    To complete your registration for event Please Upload your payment receipt on following link.
+    Hi Admin,
+{{dd($objData)}}
+    User
     @component('mail::button', ['url' => 'http://127.0.0.1:8000/upload/receipt/'.$objData['eventparticipant']->id])
         Upload
     @endcomponent
