05bfce16c70ee5c59071f310171bef5ab17904e9
adityamonad <aditya.chouhan@monadinfotech.com>
2020-04-20 11:19:46 +0530

    Add ticket price

--- app/Http/Model/Category.php
+++ app/Http/Model/Category.php
@@ -3,6 +3,7 @@
 namespace App\Http\Model;
 
 use App\Ticket;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
@@ -14,12 +15,25 @@ class Category extends Model
         return $this->hasOne('App\Http\Model\Events', 'id', 'event_id');
     }
 
-    public function ticket(){
-        return $this->hasMany(new Ticket(),  'category_id', 'id');
+    public function getEventPrice()
+    {
+        $sql = '';
+        $arrObjTickets = $this->ticket;
+
+        foreach ($arrObjTickets as $ticket) {
+            if ($ticket->quantity > 0 && Carbon::now()->toDateString() >= Carbon::parse($ticket->start_date)->toDateString() && Carbon::now()->toDateString() <= Carbon::parse($ticket->end_date)->toDateString()) {
+                return $ticket;
+            } else if ($ticket->quantity > 0 && $ticket->name == 'normal') {
+                return $ticket;
+            } else if ($ticket->quantity > 0 && $ticket->name == 'late') {
+                return $ticket;
+            }
+        }
+        return '';
     }
 
-    public function getEventPrice(){
-       return $this->ticket()->where('quantity', '>' ,'0')->whereDate('start_date', '<=', date("Y-m-d"))
-            ->whereDate('end_date', '>=', date("Y-m-d"))->first();
+    public function ticket()
+    {
+        return $this->hasMany(new Ticket(), 'category_id', 'id');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    {{--    <script--}}
-    {{--        src="https://code.jquery.com/jquery-3.4.1.js"--}}
-    {{--        integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="--}}
-    {{--        crossorigin="anonymous"></script>--}}
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
@@ -26,11 +23,11 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/demo.css') }}">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    {{--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">--}}
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
 
-    <link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+    {{--    <link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">--}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"
           type="text/css">
 </head>
