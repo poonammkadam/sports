1da3fa567f8806f7cd8432234cc9924576a69e7c
poonamk97monad <poonam.kadam@monadinfotech>
2020-05-11 22:13:38 +0530

    update email func

--- resources/views/mail/registered.blade.php
+++ resources/views/mail/registered.blade.php
@@ -1,14 +1,14 @@
 @component('mail::message')
     <html>
-    Hi {{($objData['user'])->name}}
+    Hi poonam
 
 
     To complete your registration for event Please Upload your payment receipt on following link.
-    @component('mail::button', ['url' => 'http://127.0.0.1:8000/upload/receipt/'.$objData['eventparticipant']->id])
-        Upload
-    @endcomponent
+    {{--    @component('mail::button', ['url' => 'http://127.0.0.1:8000/upload/receipt/'.$objData['eventparticipant']->id])--}}
+    {{--        Upload--}}
+    {{--    @endcomponent--}}
 
     Thanks,
-    {{ config('app.name') }}
+    {{--    {{ config('app.name') }}--}}
     </html>
 @endcomponent
